95c66b3e6c42f9455fab273fd0e53c9ffc7798e0
TanjimaKothiya <34907424+TanjimaKothiya@users.noreply.github.com>
2023-03-18 19:45:22 +0530

    Initial Commit

---
+++ ExpandedRow/ExpandedRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandedRowApp.swift
+//  ExpandedRow
+//
+//  Created by tanjima on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpandedRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
